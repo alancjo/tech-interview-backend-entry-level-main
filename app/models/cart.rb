f8cb1ce59ab1aa18b099d962fb13099f99ce3740
Alan Oliveira <alancjobm@gmail.com>
2025-02-12 14:46:01 -0300

    feat: cart validates and abandoned/removed logic

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,17 @@
 class Cart < ApplicationRecord
-  validates_numericality_of :total_price, greater_than_or_equal_to: 0
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
+  validates :status, presence: true
+
+  enum status: { active: 0, abandoned: 1 }
+
+  def total_price
+    cart_items.sum(&:total_price)
+  end
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  def mark_as_abandoned!
+    update!(status: :abandoned)
+  end
 end
