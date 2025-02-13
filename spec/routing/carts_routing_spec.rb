8a66cb8a06702d54bfd65d3d4469d1bd55a2eb0b
Alan Oliveira <alancjobm@gmail.com>
2025-02-12 22:14:15 -0300

    test: implement test

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -6,12 +6,41 @@ RSpec.describe CartsController, type: :routing do
       expect(get: '/cart').to route_to('carts#show')
     end
 
-    it 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
-    end
-
     it 'routes to #add_item via POST' do
       expect(post: '/cart/add_item').to route_to('carts#add_item')
     end
+
+    it 'routes to #remove_item via DELETE' do
+      expect(delete: '/cart/1').to route_to(
+        controller: 'carts',
+        action: 'remove_item',
+        product_id: '1'
+      )
+    end
+
+    it 'routes to #update_item via PATCH' do
+      expect(patch: '/cart/item/1').to route_to(
+        controller: 'carts',
+        action: 'update_item',
+        cart_item_id: '1'
+      )
+    end
+
+    it 'does not route to #index' do
+      expect(get: '/carts').not_to be_routable
+    end
+
+    it 'does not route to #edit' do
+      expect(get: '/cart/edit').not_to be_routable
+    end
+
+    it 'does not route to #update' do
+      expect(put: '/cart').not_to be_routable
+      expect(patch: '/cart').not_to be_routable
+    end
+
+    it 'does not route to #destroy' do
+      expect(delete: '/cart').not_to be_routable
+    end
   end
-end 
+end
